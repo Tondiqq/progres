2701974fac16dab31f2cc24f82527007b26b7f7d
Tondiqq <antoninstus@gmail.com>
2025-01-27 09:01:16 +0100

    vygenerovane vety a random slova pod sebou

--- script.js
+++ script.js
@@ -11,5 +11,23 @@ function generujSlovo(pocetPismen) {
     return slovo;
 }
 
-console.log(generujSlovo(8));
-console.log(generujSlovo(6));
+function generujVetu(pocetSlov)
+{
+    let i = 0;
+    let veta = "";
+    while (i < pocetSlov) {
+        let slovo = generujSlovo(Math.random() * 10 + 1);
+        veta += slovo + " ";
+        i++;
+    }
+    return veta;
+}
+
+// Generování deseti vět
+let i = 0;
+while (i<5) {
+    console.log(generujVetu(Math.random() * 6 + 1));
+    i++;
+}
+console.log(generujVetu(8));
+console.log(generujVetu(6));
